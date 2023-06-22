a98f87187919a813e94260ade3e4b433dcdb7a19
Вадим Тараш <r9utw@mail.ru>
2023-06-22 11:16:54 +0700

    add task19

---
+++ Task_19/Task_19.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите пятизначное число");
+string nums;
+nums = Console.ReadLine();
+if (nums.Length != 5) {
+    Console.WriteLine("неверное число");
+    return;
+}
+if(nums[0]==nums[4] && nums[1]==nums[3]) {
+    Console.WriteLine("число полиндром");
+} else {
+    Console.WriteLine("число не полиндром");
+}
