6216b45b94413c413ac63d2a1ed3ff414d6b4591
Вадим Тараш <r9utw@mail.ru>
2023-06-22 11:18:28 +0700

    add task21

---
+++ Task_21/Task_21.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] a;
+int[] b;
+double distance;
+
+System.Console.WriteLine("Введите координаты точки a:x,y,z");
+a = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
+
+System.Console.WriteLine("Введите координаты точки b:x,y,z");
+b = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
+
+distance = Math.Sqrt(Math.Pow((a[0]-b[0]), 2) + Math.Pow((a[1]-b[1]), 2) + Math.Pow((a[2]-b[2]), 2));
+
+System.Console.WriteLine("Расстояние");
+System.Console.WriteLine(Math.Round(distance, 3));
+
